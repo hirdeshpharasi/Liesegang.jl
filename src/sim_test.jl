2b54c574c7a253c205e157e85936ef1ea5c8c748
spiralizing <spiralizing@gmail.com>
2016-12-06 18:25:33 -0600

    finishing the simulation code.

--- src/sim_test.jl
+++ src/sim_test.jl
@@ -3,14 +3,16 @@
 #on the Stochastic Rotation Dynamics model.
 #loading the package Liesegang.jl
 using Liesegang
+using Plots #plotting package
 ################################################################################
 #defining the parameters
 Lx = 10; Ly = 10 #size of the space
 dim = [Lx,Ly]
 mass = 1.0
-np = 100 #number of particles
-Tr = 1.0 #reference temperature
+np = 500 #number of particles
+Tr = 0.5 #reference temperature
 τ = 0.01
+tmax = 100
 ################################################################################
 ###########                       INITIALIZING                       ###########
 
@@ -25,10 +27,16 @@ boxes = [box() for _ in 1:(Lx * Ly)]
 ################################################################################
 #########################    now the simulation...   ###########################
 #computing the momentum of each box
-box_vel(parts,boxes)
-#moving the particles
-α = 130.0 #fixing the angle just for the test
-#rotating...
-parts_vels!(parts, boxes, α)
-#now getting the new positions of the particles
-getpos_pbc!(parts, τ, dim)
+anim = @animate for _ in 1:tmax
+    box_vel(parts,boxes)
+    #moving the particles
+    α = 130.0 #fixing the angle just for the test
+    #rotating...
+    parts_vels!(parts, boxes, α)
+    #now getting the new positions of the particles
+    getpos_pbc!(parts, τ, dim)
+    x = [parts[i].pos[1] for i in 1:np]
+    y = [parts[i].pos[2] for i in 1:np]
+    scatter(x,y)
+end
+gif(anim, "test.gif", fps = 10)
